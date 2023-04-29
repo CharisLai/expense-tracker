a9a7e15e320d57200f90117ca777ceba99501fa4
CharisLai <griffin8418@gmail.com>
2023-04-29 23:26:01 +0800

    feat: Add handlebars and edit new index main hbs

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 const app = express()
 const port = 3000
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
 const db = mongoose.connection
@@ -16,10 +19,18 @@ db.once('open', () => {
     console.log('mongodb connected!')
 })
 
+// routes
 app.get('/', (req, res) => {
-    res.send('test')
+    res.render('index')
 })
 
+app.get('/new', (req, res) => {
+    res.render('new')
+})
+
+app.get('/edit', (req, res) => {
+    res.render('edit')
+})
 app.listen(3000, () => {
     console.log('App is running on http://localhost:3000')
 })
