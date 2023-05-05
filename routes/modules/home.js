a943606630917db328eecd16aeb1930633281b71
CharisLai <griffin8418@gmail.com>
2023-05-06 00:14:53 +0800

    feat: loading record.json with hbs and modify index.hbs

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
-
+const data = require('../../models/seeds/record.json')
 const Expense = require('../../models/expense')
 
 router.get('/', (req, res) => {
     //取得所有資料
-    Expense.find()
-        .lean()
-        .then(data => res.render('index', { data }))
-        .catch(error => console.log(error))
+    res.render('index', { data: data.results })
+    // Expense.find()
+    //     .lean()
+    //     .then(data => res.render('index', { data }))
+    //     .catch(error => console.log(error))
 
 })
 
