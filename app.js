416957e4f677cc1ff183ad7048b99a19b19ae3e3
CharisLai <griffin8418@gmail.com>
2023-05-07 21:32:33 +0800

    feat Add user login via passport and logout feature

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const usePassport = require('./config/passport')
 const routes = require('./routes')
+
 require('./config/mongoose')
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -18,17 +20,21 @@ app.engine('hbs', exphbs.engine({
         match: (a, b) => a === b,
     },
 }))
-app.use(express.static('public'))
 app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
-app.use(routes)
+app.use(express.static('public'))
 // express-session
 app.use(session({
     secret: 'What is this?',
     resave: false,
     saveUninitialized: true
 }))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+// Passport
+usePassport(app)
+app.use(routes)
+
 
 app.listen(PORT, () => {
     console.log(`App is running on http://localhost:${PORT}`)
--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 const User = require('../../models/user')
+
 
 router.get('/login', (req, res) => {
     res.render('login')
 })
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/user/login'
+}))
+
 
 router.get('/register', (req, res) => {
     res.render('register')
 })
-
 router.post('/register', (req, res) => {
     const { name, email, password, confirmPassword } = req.body
     User.findOne({ email }).then(user => {
@@ -32,5 +38,15 @@ router.post('/register', (req, res) => {
         }
     })
         .catch(error => console.log(error))
+})
+
+// Logout
+router.get('/logout', (req, res) => {
+    req.logout(function (error) {
+        if (error) {
+            return next(error)
+        }
+        res.redirect('/user/login')
+    })
 })
 module.exports = router
